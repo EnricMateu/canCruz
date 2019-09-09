4e3081866f1f918e5111e49477ec3d1f14110d8c
Isa Almena <isa@admins-MBP.home>
2019-09-09 15:33:27 +0200

    Front bien ordenado con sass

---
+++ resources/views/_sleep.blade.php
@@ -0,0 +1,8 @@
+<h1>
+                Dormir
+            </h1>
+            <div class="col-md-12 sleep-room">
+                <div class="col-md-10 room-slider">
+
+                </div>
+            </div>
